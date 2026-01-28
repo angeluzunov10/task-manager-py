8d5506237d4426d9391aa88ce11e52b521eb3696
angeluzunov10 <angel_d_u@abv.bg>
2026-01-28 19:13:04 +0200

    Model: Establish User table and One-to-Many relationship with tasks

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from database import Base
+from sqlalchemy.orm import relationship # за връзки между обектите/таблиците
 
 
 class BaseTask(Base): # наследяваме от Base, който е в database.py, за да може SQLAlchemy да работи с този клас(модела се прави от базата)
@@ -7,12 +8,16 @@ class BaseTask(Base): # наследяваме от Base, който е в datab
     id = Column(Integer, primary_key=True, index=True) # уникален идентификатор за всяка задача PK
     title = Column(String, index=True)
     description = Column(String)
+    owner_id = Column(Integer, ForeignKey("users.id")) # Това е външен ключ, който сочи към id-то в таблицата users
     status = Column(String, default='Pending') # сменяме го така, защото вече не е private променлива, а SQLAlchemy колонка
     type = Column(String) # за да различаваме типовете задачи (WorkTask, PersonalTask)
 
     # Специфични полета за наследниците
     deadline = Column(String, nullable=True)
     priority = Column(String, nullable=True)
+
+    # Връзка: Всяка задача принадлежи на един потребител, а relationship ни позволява да достъпим потребителя чрез task.owner
+    owner = relationship("User", back_populates="tasks")
 
     @staticmethod
     def from_dict(data):
@@ -95,3 +100,15 @@ class PersonalTask(BaseTask):
     
     def __str__(self):
         return super().__str__() + f" (Priority: {self.priority})"
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    hashed_password = Column(String) # никога не съхраняваме пароли в plain text!
+
+    # Връзка: Един потребител има много задачи
+    # back_populates казва на SQLAlchemy как се казва атрибутът в другия модел
+    tasks = relationship("BaseTask", back_populates="owner")
+
