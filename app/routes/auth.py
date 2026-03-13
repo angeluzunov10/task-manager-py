23650bb95b0f8ae8779499b8ed58ff3e36b92ad6
angeluzunov10 <angel_d_u@abv.bg>
2026-03-13 15:27:49 +0200

    ci: fix missing env variables in github

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -15,7 +15,7 @@ load_dotenv()
 # Настройки за сигурност
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "30"))
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 # Настройваме шаблоните
